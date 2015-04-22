e8a4d68ff9408849f30eb3a3f2eb892e0fc844a5
Troy Dai <troy.dai@outlook.com>
2015-04-22 15:52:09 -0700

    Add unit tests

---
+++ test/Microsoft.Framework.Runtime.Roslyn.Tests/AfterCompileContextFacts.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Microsoft.Framework.Runtime.Roslyn.Tests
+{
+    public class AfterCompileContextFacts
+    {
+        private AfterCompileContext _target;
+
+        public AfterCompileContextFacts()
+        {
+            _target = new AfterCompileContext();
+        }
+
+        [Fact]
+        public void DefaultContructor()
+        {
+            // nothing is set
+            Assert.Null(_target.Compilation);
+            Assert.Null(_target.Diagnostics);
+            Assert.Null(_target.ProjectContext);
+            Assert.Null(_target.AssemblyStream);
+            Assert.Null(_target.SymbolStream);
+            Assert.Null(_target.XmlDocStream);
+        }
+
+        [Fact]
+        public void SetCompilation()
+        {
+            var compilation = CSharpCompilation.Create("nothing");
+
+            _target.Compilation = compilation;
+            Assert.Equal(compilation, _target.Compilation);
+        }
+    }
+}
---
+++ test/Microsoft.Framework.Runtime.Roslyn.Tests/BeforeCompileContextFacts.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Microsoft.Framework.Runtime.Roslyn.Tests
+{
+    public class BeforeCompileContextFacts
+    {
+        private BeforeCompileContext _target;
+
+        public BeforeCompileContextFacts()
+        {
+            _target = new BeforeCompileContext();
+        }
+
+        [Fact]
+        public void DefaultContructor()
+        {
+            // nothing is set
+            Assert.Null(_target.Compilation);
+            Assert.Null(_target.Diagnostics);
+            Assert.Null(_target.MetadataReferences);
+            Assert.Null(_target.ProjectContext);
+            Assert.Null(_target.Resources);
+        }
+
+        [Fact]
+        public void SetCompilation()
+        {
+            var compilation = CSharpCompilation.Create("nothing");
+
+            _target.Compilation = compilation;
+            Assert.Equal(compilation, _target.Compilation);
+        }
+    }
+}
